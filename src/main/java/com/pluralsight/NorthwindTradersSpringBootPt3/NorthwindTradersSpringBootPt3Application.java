393239816f456fed8bd928eb66bcbcb54154d89e
nati-tewolde <natisnewemail@gmail.com>
2025-12-05 13:12:34 -0500

    Initial commit

---
+++ src/main/java/com/pluralsight/NorthwindTradersSpringBootPt3/NorthwindTradersSpringBootPt3Application.java
@@ -0,0 +1,13 @@
+package com.pluralsight.NorthwindTradersSpringBootPt3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthwindTradersSpringBootPt3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthwindTradersSpringBootPt3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluralsight/NorthwindTradersSpringBootPt3/NorthwindTradersSpringBootPt3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.NorthwindTradersSpringBootPt3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NorthwindTradersSpringBootPt3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
